a69276a79e2a395387b9c5d8e38a84e8475d48d7
jorgecasgut <jorgetoda8@gmail.com>
2017-03-30 00:45:51 -0600

    Agragando las variables y operaciones para que funcione el calculo de IMC

--- app/src/main/java/com/example/diego/imc/IMC.java
+++ app/src/main/java/com/example/diego/imc/IMC.java
@@ -2,6 +2,10 @@ package com.example.diego.imc;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class IMC extends AppCompatActivity {
 
@@ -9,5 +13,50 @@ public class IMC extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_imc);
+
+        final EditText peso, altura;
+
+        Button Calcular = (Button) findViewById(R.id.btncalcular);
+        peso = (EditText) findViewById(R.id.txtxpeso);
+        altura =(EditText) findViewById(R.id.txtaltura);
+
+        Calcular.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(peso.getText().toString().trim().length() == 0){
+                    peso.requestFocus();
+
+                    Toast.makeText(IMC.this,
+                            "LLenar el peso",Toast.LENGTH_SHORT).show();
+
+                }
+                else if (altura.getText().toString().trim().length() == 0){
+                    altura.requestFocus();
+
+                    Toast.makeText(IMC.this,
+                            "LLenar el altura",Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    float kg = Float.parseFloat(peso.getText().toString());
+                    float cm = Float.parseFloat(altura.getText().toString());
+                    float m = (cm / 100);
+                    float imc = (kg / (m * m));
+
+                    if(imc < 18.5){
+                        Toast.makeText(IMC.this, "Tu IMC es: " + imc + "\n" +
+                                "Estas delgado", Toast.LENGTH_SHORT).show();
+                    }else if (imc < 24.9){
+                        Toast.makeText(IMC.this, "Tu IMC es: " + imc + "\n" +
+                                "Tu peso es normal", Toast.LENGTH_SHORT).show();
+                    }else if  (imc < 29.9){
+                        Toast.makeText(IMC.this, "Tu IMC es: " + imc + "\n" +
+                                "Tienes sobrepeso", Toast.LENGTH_SHORT).show();
+                    }else if (imc > 30){
+                        Toast.makeText(IMC.this, "Tu IMC es: " + imc + "\n" +
+                                "Padeces obesidad", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
     }
 }
--- app/src/main/java/com/example/diego/imc/calculo_imc.java
+++ app/src/main/java/com/example/diego/imc/calculo_imc.java
@@ -1,8 +1,11 @@
 package com.example.diego.imc;
 
+import android.widget.EditText;
+
 /**
  * Created by Diego on 28/03/2017.
  */
 
 public class calculo_imc {
+    EditText peso, altura;
 }
